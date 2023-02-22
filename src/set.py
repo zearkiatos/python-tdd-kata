7bf38358adafff54552203c8dd55e3736c2c4df9
zearkiatos <caprilespe@outlook.com>
2023-02-22 00:07:55 -0300

    Case 4: Calculate average with a set of n elements 🔵

--- src/set.py
+++ src/set.py
@@ -3,9 +3,7 @@ class Set:
         self.__set = set
 
     def average(self):
-        if len(self.__set) == 1:
-            return (self.__set[0])
-        elif len(self.__set) == 2:
-            return (self.__set[0] + self.__set[1]) / 2
+        if len(self.__set) > 0:
+            return sum(self.__set) / len(self.__set)
         else:
             return None
