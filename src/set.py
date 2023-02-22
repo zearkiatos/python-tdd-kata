579e08c5447442d31807c7d2a7b069483a7dd26c
zearkiatos <caprilespe@outlook.com>
2023-02-21 23:58:18 -0300

    Case 3: calculate average with a set with two elements 🟢

--- src/set.py
+++ src/set.py
@@ -5,5 +5,7 @@ class Set:
     def average(self):
         if len(self.__set) == 1:
             return (self.__set[0])
+        elif len(self.__set) == 2:
+            return (self.__set[0] + self.__set[1]) / 2
         else:
             return None
