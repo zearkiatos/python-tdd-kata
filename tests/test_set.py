38cdfd659bc96fa0e48e79b757235dd72be5b271
zearkiatos <caprilespe@outlook.com>
2023-02-21 21:31:23 -0300

    Case 2: calculate average with an empty set 🔴

--- tests/test_set.py
+++ tests/test_set.py
@@ -4,4 +4,10 @@ from src.set import Set
 class TestSet(unittest.TestCase):
     def test_set_empty(self):
         set = Set([])
+        
         self.assertIsNone(set.average())
+
+    def test_set_with_an_element(self):
+        set = Set([5])
+        
+        self.assertEqual(set.average(), 5)
